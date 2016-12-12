743d855d029c21a7bddac704ae904ccb8ba50a9a
wasabi-peanut <peanut.covered.in.wasabi@gmail.com>
2016-12-11 21:43:23 -0700

    added checking set up VC

---
+++ BingoNight 2.2/BingoNight 2.2/CheckingSetUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  CheckingSetUpViewController.h
+//  BingoNight 2.2
+//
+//  Created by Schor on 12/11/16.
+//  Copyright © 2016 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckingSetUpViewController : UIViewController
+
+@end
