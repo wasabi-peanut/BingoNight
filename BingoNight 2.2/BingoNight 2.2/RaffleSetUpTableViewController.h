b716c5da6c8dff1e1603d8e737856b5ed92e1672
mr-crockpot <adamschor@Adams-MacBook-Air.local>
2017-02-17 17:08:40 -0700

    nothing

--- BingoNight 2.2/BingoNight 2.2/RaffleSetUpTableViewController.h
+++ BingoNight 2.2/BingoNight 2.2/RaffleSetUpTableViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface RaffleSetUpTableViewController : UITableViewController <UITextViewDelegate>
-
+//latest version
 
 
 @property (strong,nonatomic) NSMutableArray *arrayRaffleItems;
